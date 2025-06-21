18f02f4b551734de6411549933c86291d2689512
VONG Jeremy <gnov.jeremy@gmail.com>
2025-06-21 15:12:28 +0200

    Testing github action test

---
+++ src/test/java/me/stky/relaytd/RelaytdApplicationTests.java
@@ -0,0 +1,32 @@
+package me.stky.relaytd;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Slf4j
+class RelaytdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @BeforeAll
+    static void setup() {
+        log.info("@BeforeAll - executes once before all test methods in this class");
+    }
+
+    @BeforeEach
+    void init() {
+        log.info("@BeforeEach - executes before each test method in this class");
+    }
+    @Test
+    void rdn(){
+        assertEquals(5,2+3);
+    }
+}
